39701c018def3b95bd10008eaa593148f207d669
Artur Bodera <abodera@gmail.com>
2013-07-10 19:20:17 +0200

    Add AbstractResult and 3 simple Result classes for instant use.

---
+++ src/ZendDiagnostics/Result/Failure.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace ZendDiagnostics\Result;
+
+class Failure extends AbstractResult implements FailureInterface {}
---
+++ src/ZendDiagnostics/Result/Warning.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+namespace ZendDiagnostics\Result;
+
+class Warning extends AbstractResult implements WarningInterface {}
