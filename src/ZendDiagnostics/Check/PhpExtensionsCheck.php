815ab39860e01b98858805cc403a350a5f4758e0
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-07-16 16:21:02 +0200

    ported all Checks from LiipMonitor (fixes #3)

---
+++ src/ZendDiagnostics/Check/PhpExtensionsCheck.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendDiagnostics\Check;
+
+use ZendDiagnostics\Result\Failure;
+use ZendDiagnostics\Result\Success;
+
+class PhpExtensionsCheck extends AbstractCheck
+{
+    protected $extensions;
+
+    /**
+     * @param array $extensions List of extensions names you want to test availability
+     */
+    public function __construct($extensions)
+    {
+        $this->extensions = $extensions;
+    }
+
+    /**
+     * @see ZendDiagnostics\CheckInterface::check()
+     */
+    public function check()
+    {
+        $missingExtensions = array();
+        foreach ($this->extensions as $extension) {
+            if (!extension_loaded($extension)) {
+                $missingExtensions[] = $extension;
+            }
+        }
+
+        if (count($missingExtensions) > 0) {
+            return new Failure(sprintf('The following extensions are not missing: "%s"', implode('", "', $missingExtensions)));
+        }
+
+        return new Success();
+    }
+
+    /**
+     * @see Liip\MonitorBundle\Check\CheckInterface::getName()
+     */
+    public function getName()
+    {
+        return "PHP Extensions Health Check";
+    }
+}
