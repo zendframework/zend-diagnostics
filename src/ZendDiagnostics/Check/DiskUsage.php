1ef3f571ca07cb932c4a4d91e324124b2768ea8f
Kevin Bond <kevinbond@gmail.com>
2014-02-12 20:04:28 -0500

    fix cs

--- src/ZendDiagnostics/Check/DiskUsage.php
+++ src/ZendDiagnostics/Check/DiskUsage.php
@@ -8,23 +8,62 @@ namespace ZendDiagnostics\Check;
 use ZendDiagnostics\Result\Failure;
 use ZendDiagnostics\Result\Success;
 use ZendDiagnostics\Result\Warning;
+use InvalidArgumentException;
 
 /**
  * Checks to see if the disk usage is below warning/critical percent thresholds
  */
-class DiskUsage implements CheckInterface
+class DiskUsage extends AbstractCheck implements CheckInterface
 {
+    /**
+     * Percentage that will cause a warning.
+     *
+     * @var int
+     */
     protected $warningThreshold;
+
+    /**
+     * Percentage that will cause a fail.
+     *
+     * @var int
+     */
     protected $criticalThreshold;
+
+    /**
+     * The disk path to check.
+     *
+     * @var string
+     */
     protected $path;
 
     /**
-     * @param int $warningThreshold
-     * @param int $criticalThreshold
-     * @param int $path
+     * @param int                       $warningThreshold  A number between 0 and 100
+     * @param int                       $criticalThreshold A number between 0 and 100
+     * @param string                    $path              The disk path to check, i.e. '/tmp' or 'C:' (defaults to /)
+     * @throws InvalidArgumentException
      */
-    public function __construct($warningThreshold, $criticalThreshold, $path)
+    public function __construct($warningThreshold, $criticalThreshold, $path = '/')
     {
+        if (!is_string($path)) {
+            throw new InvalidArgumentException('Invalid disk path argument - expecting a string');
+        }
+
+        if (!is_string($warningThreshold)) {
+            throw new InvalidArgumentException('Invalid warningThreshold argument - expecting an integer');
+        }
+
+        if (!is_string($criticalThreshold)) {
+            throw new InvalidArgumentException('Invalid criticalThreshold argument - expecting an integer');
+        }
+
+        if ($warningThreshold > 100 || $warningThreshold < 0) {
+            throw new InvalidArgumentException('Invalid warningThreshold argument - expecting an integer between 1 and 100');
+        }
+
+        if ($criticalThreshold > 100 || $criticalThreshold < 0) {
+            throw new InvalidArgumentException('Invalid criticalThreshold argument - expecting an integer between 1 and 100');
+        }
+
         $this->warningThreshold = (int) $warningThreshold;
         $this->criticalThreshold = (int) $criticalThreshold;
         $this->path = $path;
@@ -43,21 +82,13 @@ class DiskUsage implements CheckInterface
         $dp = ($du / $dt) * 100;
 
         if ($dp >= $this->criticalThreshold) {
-            return new Failure(sprintf('Disc usage too high: %2d percentage.', $dp));
+            return new Failure(sprintf('Disk usage too high: %2d percent.', $dp));
         }
 
         if ($dp >= $this->warningThreshold) {
-            return new Warning(sprintf('Disc usage high: %2d percentage.', $dp));
+            return new Warning(sprintf('Disk usage high: %2d percent.', $dp));
         }
 
-        return new Success();
-    }
-
-    /**
-     * @return string
-     */
-    public function getLabel()
-    {
-        return 'Disc Usage';
+        return new Success(sprintf('Disk usage is %2d percent.', $dp));
     }
 }
