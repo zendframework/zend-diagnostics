e1cbdca66f9168a43fda8a2455baaa430dd9e9d7
Kevin Bond <kevinbond@gmail.com>
2014-06-21 10:45:03 -0400

    added OpCacheMemory check

--- src/ZendDiagnostics/Check/ApcMemory.php
+++ src/ZendDiagnostics/Check/ApcMemory.php
@@ -5,7 +5,6 @@
 
 namespace ZendDiagnostics\Check;
 
-use InvalidArgumentException;
 use ZendDiagnostics\Result\Failure;
 use ZendDiagnostics\Result\Skip;
 use ZendDiagnostics\Result\Success;
@@ -20,48 +19,14 @@ use ZendDiagnostics\Result\Warning;
  *      license:   The PHP License, version 3.01
  *      copyright: Copyright (c) 2006-2011 The PHP Group
  */
-class ApcMemory extends AbstractCheck implements CheckInterface
+class ApcMemory extends AbstractMemoryCheck
 {
     /**
-     * Percentage that will cause a warning.
+     * APC information
      *
-     * @var int
+     * @var array
      */
-    protected $warningThreshold;
-
-    /**
-     * Percentage that will cause a fail.
-     *
-     * @var int
-     */
-    protected $criticalThreshold;
-
-    /**
-     * @param int $warningThreshold  A number between 0 and 100
-     * @param int $criticalThreshold A number between 0 and 100
-     * @throws InvalidArgumentException
-     */
-    public function __construct($warningThreshold, $criticalThreshold)
-    {
-        if (!is_numeric($warningThreshold)) {
-            throw new InvalidArgumentException('Invalid warningThreshold argument - expecting an integer');
-        }
-
-        if (!is_numeric($criticalThreshold)) {
-            throw new InvalidArgumentException('Invalid criticalThreshold argument - expecting an integer');
-        }
-
-        if ($warningThreshold > 100 || $warningThreshold < 0) {
-            throw new InvalidArgumentException('Invalid warningThreshold argument - expecting an integer between 1 and 100');
-        }
-
-        if ($criticalThreshold > 100 || $criticalThreshold < 0) {
-            throw new InvalidArgumentException('Invalid criticalThreshold argument - expecting an integer between 1 and 100');
-        }
-
-        $this->warningThreshold  = (int)$warningThreshold;
-        $this->criticalThreshold = (int)$criticalThreshold;
-    }
+    private $apcInfo;
 
     /**
      * Perform the check
@@ -83,43 +48,30 @@ class ApcMemory extends AbstractCheck implements CheckInterface
             return new Warning('APC extension is not available');
         }
 
-        if (!$info = apc_sma_info()) {
+        if (!$this->apcInfo = apc_sma_info()) {
             return new Warning('Unable to retrieve APC memory status information.');
         }
 
-        $size        = $info['num_seg'] * $info['seg_size'];
-        $available   = $info['avail_mem'];
-        $used        = $size - $available;
-        $percentUsed = ($used / $size) * 100;
-        $message     = sprintf('%.0f%% of available %s memory used.', $percentUsed, $this->formatBytes($size));
-
-        if ($percentUsed > $this->criticalThreshold) {
-            return new Failure($message);
-        }
-
-        if ($percentUsed > $this->warningThreshold) {
-            return new Warning($message);
-        }
-
-        return new Success($message);
+        return parent::check();
     }
 
     /**
-     * @param int $bytes
-     * @return string
+     * Returns the total memory in bytes
+     *
+     * @return int
      */
-    private function formatBytes($bytes)
+    protected function getTotalMemory()
     {
-        $size = 'B';
+        return $this->apcInfo['num_seg'] * $this->apcInfo['seg_size'];
+    }
 
-        foreach (array('B','KB','MB','GB') as $size) {
-            if ($bytes < 1024) {
-                break;
-            }
-
-            $bytes /= 1024;
-        }
-
-        return sprintf("%.0f %s", $bytes, $size);
+    /**
+     * Returns the used memory in bytes
+     *
+     * @return int
+     */
+    protected function getUsedMemory()
+    {
+        return $this->getTotalMemory() - $this->apcInfo['avail_mem'];
     }
 }
