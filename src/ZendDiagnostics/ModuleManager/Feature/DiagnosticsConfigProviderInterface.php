41c1d944635580e1e051b1450375e5a52c5fb4c7
root <userator@ya.ru>
2014-09-26 14:04:34 +0400

    add modulemanager feature

---
+++ src/ZendDiagnostics/ModuleManager/Feature/DiagnosticsConfigProviderInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace ZendDiagnostics\ModuleManager\Feature;
+
+/**
+ * @author kurshin
+ */
+interface DiagnosticsProviderInterface {
+
+	/**
+	 * Expected to return \Zend\ServiceManager\Config object or array to
+	 * seed such an object.
+	 *
+	 * @return array|\Zend\ServiceManager\Config
+	 */
+	public function getDiagnosticsConfig();
+}
